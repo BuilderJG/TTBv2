136ca13c5d604a9a12f6a981173af0d389740fba
BuilderJG <buijg@outlook.de>
2024-12-30 16:36:51 +0100

    Ästhetische Änderungen, Styling vom Confirm-Dialog

--- main.js
+++ main.js
@@ -85,23 +85,23 @@ function open_dialog(dialogId) { //dialogId: string = HTML-ID
 
     // Funktion, um einen simplen Dialog mit zwei Auswahlmöglichkeiten zu erstellen
 function confirm_dialog(title, desc, btn1_txt, btn1_fnc, btn2_txt, btn2_fnc) {
-    let elem_dialog = document.getElementById("confirm-popup")
-    document.getElementById("confirm-popup-title").innerHTML = title
-    document.getElementById("confirm-popup-description").innerHTML = desc
-    let btn1 = document.getElementById("confirm-popup-btn1")
+    let elem_dialog = document.getElementById("confirm-dialog")
+    document.getElementById("confirm-dialog-title").innerHTML = title
+    document.getElementById("confirm-dialog-description").innerHTML = desc
+    let btn1 = document.getElementById("confirm-dialog-btn1")
     btn1.innerHTML = btn1_txt
     btn1.onclick = function () {
         elem_dialog.close();
         btn1_fnc()
     }
-    let btn2 = document.getElementById("confirm-popup-btn2")
+    let btn2 = document.getElementById("confirm-dialog-btn2")
     btn2.innerHTML = btn2_txt
     btn2.onclick = function () {
         elem_dialog.close();
         btn2_fnc()
     }
 
-    open_dialog("confirm-popup")
+    open_dialog("confirm-dialog")
 }
 
     // leere Funktion, wird verwendet, um einen Knopf des confirm_dialog Dialogs (z.B. Abbrechen) lediglich den Dialog schließen zu lassen
