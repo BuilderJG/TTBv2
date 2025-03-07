b39a9965165eaa48da5d574b1db56f95347b21c4
BuilderJG <buijg@outlook.de>
2025-03-07 14:07:29 +0100

    Optimierung der app.py: die Namen der verfügbaren Funktionen für die Spond API werden nun in einer eigenen Liste gespeichert

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import render_template, Flask, request
 import asyncio
+from flask import render_template, Flask, request
 from spond import spond
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 app = Flask(__name__)
 
-spond_login_error_message = "Login failed. Response received: {'errorKey': 'wrongEmailOrPassword', 'message': 'Wrong email or password.'}"
+spond_api_functions = ["check_login", "get_all_groups"]
+
 
 @app.route("/")
 def index():
@@ -20,7 +21,7 @@ def page_login_spond():
 @app.route("/api/spond", methods=["POST"])
 def spond_api():
     body = request.get_json()
-    if not ("username" in body and "password" in body and "function" in body and body["function"] in ["check_login", "get_all_groups"]):
+    if not ("username" in body and "password" in body and "function" in body and body["function"] in spond_api_functions):
         return {"error": "bad-request"}, 400
     try:
         username = body["username"]
